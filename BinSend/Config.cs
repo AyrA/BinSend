8c68e74936f1bf5b05836fcb2ec0eecc41d4eb53
Kevin Gut <kevin.gut@bitm.sg>
2018-06-05 03:04:57 +0200

    Settings Structure

---
+++ BinSend/Config.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Net;
+
+namespace BinSend
+{
+    public struct Config
+    {
+        public ApiConfig ApiSettings;
+        public Template[] Templates;
+
+        public bool Valid()
+        {
+            return Templates != null &&
+                Templates.All(m => m.Valid()) &&
+                ApiSettings.Valid();
+        }
+    }
+
+    public struct Template
+    {
+        public string Name;
+        public string Value;
+
+        public bool Valid()
+        {
+            return !string.IsNullOrEmpty(Name) &&
+                !string.IsNullOrEmpty(Value);
+        }
+    }
+
+    public struct ApiConfig
+    {
+        public string IpOrHostname;
+        public int Port;
+        public string Username;
+        public string Password;
+
+        public bool Valid()
+        {
+            return !string.IsNullOrEmpty(IpOrHostname) &&
+                Port >= IPEndPoint.MinPort &&
+                Port <= IPEndPoint.MaxPort &&
+                !string.IsNullOrEmpty(Username) &&
+                !string.IsNullOrEmpty(Password);
+        }
+    }
+}
